7977517121657b11332d853831900827ad120aba
Joe <jcshilkaitis@hotmail.com>
2022-11-04 18:20:37 -0500

    initial commit

---
+++ src/main/java/dmacc/TonysAutoShopApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonysAutoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonysAutoShopApplication.class, args);
+	}
+
+}
