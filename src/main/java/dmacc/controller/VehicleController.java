54345dc6a80a66990f7033c37223a955a8ae8e0e
Joe <jcshilkaitis@hotmail.com>
2022-12-02 05:10:22 -0600

    updated vehicle controller, added vehicles to customer info page

--- src/main/java/dmacc/controller/VehicleController.java
+++ src/main/java/dmacc/controller/VehicleController.java
@@ -1,5 +1,8 @@
 package dmacc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +11,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import dmacc.beans.Customer;
 import dmacc.beans.Vehicle;
+import dmacc.repository.CustomerRepository;
 import dmacc.repository.VehicleRepository;
 
 /**
@@ -19,44 +24,59 @@ import dmacc.repository.VehicleRepository;
 @Controller
 public class VehicleController {
 	@Autowired
-	VehicleRepository repo;
+	VehicleRepository vehicleRepo;
+	
+	@Autowired
+	CustomerRepository customerRepo;
+
 	
 	@GetMapping("/viewAllVehicles")
 	public String viewAllVehicles(Model model) {
-		model.addAttribute("vehicle", repo.findAll());
+		model.addAttribute("vehicle", vehicleRepo.findAll());
 		return "viewAllVehicles";
 		}
 	
-	@GetMapping("/addVehicle")
-	public String addVehicle(Model model) {
+	@GetMapping("/addVehicle/{custId}")
+	public String addVehicle(@PathVariable("custId") long custId,Model model) {
+		Customer c = customerRepo.findById(custId).orElse(null);
 		Vehicle v = new Vehicle();
+		model.addAttribute("customer", c);
 		model.addAttribute("newVehicle", v);
 		return "addVehicle";
 	}
 	
-	@PostMapping("/addVehicle")
-	public String addVehicle(@ModelAttribute Vehicle v, Model model) {
-		repo.save(v);
-		return viewAllVehicles(model);
+	@PostMapping("/addVehicle/{custId}")
+	public String addVehicle(@PathVariable("custId") long custId, @ModelAttribute Vehicle v, Customer c, Model model) {
+		v.setCustomer(customerRepo.findById(custId).orElse(null));
+		vehicleRepo.save(v);
+		
+		c = customerRepo.findById(custId).orElse(null);
+		List<Vehicle> vehicle = new ArrayList<Vehicle>();
+		vehicle.add(v);
+		c.setVehicles(vehicle);
+		customerRepo.save(c);
+		
+		return "viewAllCustomers";
+
 	}
 	
 	@GetMapping("/editVehicle/{id}")
 	public String editVehicle(@PathVariable("id") long id, Model model) {
-		Vehicle v = repo.findById(id).orElse(null);
+		Vehicle v = vehicleRepo.findById(id).orElse(null);
 		model.addAttribute("newVehicle", v);
 		return "editVehicle";
 	}
 
 	@PostMapping("/updateVehicle/{id}")
 	public String updateVehicle(Vehicle v, Model model) {
-		repo.save(v);
+		vehicleRepo.save(v);
 		return viewAllVehicles(model);
 	}
 
 	@GetMapping("/deleteVehicle/{id}")
 	public String deleteVehicle(@PathVariable("id") long id, Model model) {
-		Vehicle v = repo.findById(id).orElse(null);
-		repo.delete(v);
+		Vehicle v = vehicleRepo.findById(id).orElse(null);
+		vehicleRepo.delete(v);
 		return viewAllVehicles(model);
 	}
 }
